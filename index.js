1a94a0e1a312f36028b46f0267c7af0856102a97
TejwantKaur <kaurtejwant998@gmail.com>
2025-11-25 18:08:56 +0530

    sample data added in db

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require("express");
-const mongoose = require('mongoose');
+const express = require("express")
+const mongoose = require('mongoose')
 const path = require("path")
+const Chat = require("./models/chat.js")
 
 const app = express();
-app.set("views", path.join(__dirname,"views"));
-app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"))
+app.set("view engine","ejs")
 
 main()
   .then(()=> console.log("DB Connected Successfully"))
   .catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whasapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/whasapp')
 }
+
+
+
+
 
 
 app.get("/",(req,res)=>{
