eafbb92ba06f676cd19bf23df2bb9fafa07cf264
TejwantKaur <kaurtejwant998@gmail.com>
2025-11-25 18:43:05 +0530

    successfully new route created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Chat = require("./models/chat.js")
 const app = express();
 app.set("views", path.join(__dirname,"views"))
 app.set("view engine","ejs")
+app.use(express.urlencoded({ extended: true }))
 
 main()
   .then(()=> console.log("DB Connected Successfully"))
@@ -14,8 +15,29 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whasapp')
 }
 
+// index route
+app.get("/chats", async (req,res)=>{
+  let chats = await Chat.find();
+  res.render("index.ejs",{ chats });
+})
 
+// new route
+app.get("/chats/new", (req,res)=>{
+  res.render("new.ejs")
+})
 
+// create route
+app.post("/chats", (req,res)=>{
+  let {from, to, msg} = req.body
+  let newChat = new Chat ({
+    from: from, msg: msg, to:to,
+    created_at: new Date()
+  });
+  newChat.save()
+  .then((res)=> { console.log(res)})
+  .catch((err)=> { console.log(err)})
+  res.redirect("/chats")
+});
 
 
 
--- models/chat.js
+++ models/chat.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
     from: { type: String, required: true },
     to: { type: String, required: true },
-    msge: { type: String, maxLength: 50 },
+    msg: { type: String, maxLength: 50 },
     created_at: { type: Date, required: true},
 })
 
